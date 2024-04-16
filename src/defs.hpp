4cbd85378808d674983e65dd929edecf94a85693
ratgrandmother <118036429+RatGrandmother@users.noreply.github.com>
2024-04-16 13:32:31 +0000

    some comments were added

--- src/defs.hpp
+++ src/defs.hpp
@@ -1,7 +1,9 @@
 // good code lol
 // use pragma once next time if you dont wanna have circular dependancy 
 
-#define SCREEN_WIDTH 800
+
+// these macros are going to throw errors if we dont put them in CMakeLists.txt ( so put your headers in there kids)
+#define SCREEN_WIDTH 800 
 #define SCREEN_HEIGHT 600
 
 #define MAP_SIZE 96
